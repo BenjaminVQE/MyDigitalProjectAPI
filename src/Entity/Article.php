14f5308a1f8242d1fe709749ed4f106ba39f6d47
Benjamin <vaique.benjamin@gmail.com>
2025-02-11 09:33:21 +0100

    Added validation + route GET POST DELETE PUT

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,13 +2,25 @@
 
 namespace App\Entity;
 
-use App\Repository\ArticleRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Carts;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Post;
 use Doctrine\DBAL\Types\Types;
+use ApiPlatform\Metadata\Delete;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ApiResource]
+#[Delete]
+#[Get]
+#[Post]
+#[Put]
 class Article
 {
     #[ORM\Id]
@@ -17,15 +29,19 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    #[Assert\NotBlank]
     private ?string $width = null;
 
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    #[Assert\NotBlank]
     private ?string $height = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $matter = null;
 
     /**
